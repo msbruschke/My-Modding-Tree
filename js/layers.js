6040a8e86e78b084aa99d8725ee0d6ed36507b39
msbruschke <103210835+msbruschke@users.noreply.github.com>
2025-07-01 22:53:36 -0700

    phleg

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,42 @@
 addLayer("c", {
     name: "choleric", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "c", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#FCD303",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "choleric points", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+        11: {
+                title: "Make this whatever you want!",
+                description: "Double your point gain.",
+                cost: new Decimal(1),
+        }
+    }
+})
+
+addLayer("p", {
+    name: "phlegmatic", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "p", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,7 +44,7 @@ addLayer("c", {
     }},
     color: "#FCD303",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "phlegmatic points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
