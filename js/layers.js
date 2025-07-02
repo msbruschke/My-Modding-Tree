07ba66f5ba44e3686715de6a25437f6866dfe5a1
msbruschke <103210835+msbruschke@users.noreply.github.com>
2025-07-01 22:49:47 -0700

    first upgrade

--- js/layers.js
+++ js/layers.js
@@ -1,12 +1,12 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("c", {
+    name: "choleric", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "c", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#FCD303",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
@@ -24,5 +24,12 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+        11: {
+                title: "Make this whatever you want!",
+                description: "Double your point gain.",
+                cost: new Decimal(1),
+        }
+    }
 })
